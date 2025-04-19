8cb2f6e8422111224f1d76cf681051966ed40848
Jason Rogena <jason@rogena.me>
2025-04-19 19:31:59 +0100

    Fix mime type testing differences between OSs
    Some mime types are expected to be different in Linux, MacOS and Windows.
    For these mime types, write separate test functions, one for each OS.
    
    Testing
    =======
    
    Wait for PR tests to pass.
    
    Signed-off-by: Jason Rogena <jason@rogena.me>

--- src/mime_type/tests.rs
+++ src/mime_type/tests.rs
@@ -16,10 +16,6 @@ fn test_is_of_type() {
         (File::new(Path::new("tests/files/image/png")), "image/png"),
         (File::new(Path::new("tests/files/image/tiff")), "image/tiff"),
         (
-            File::new(Path::new("tests/files/audio/wav")),
-            "application/x-riff",
-        ),
-        (
             File::new(Path::new("tests/files/archive/x-7z-compressed")),
             "application/x-7z-compressed",
         ),
@@ -43,6 +39,33 @@ fn test_is_of_type() {
             File::new(Path::new("tests/files/archive/zip")),
             "application/zip",
         ),
+    ];
+    for cur_case in ok_test_cases.iter() {
+        assert_eq!(cur_case.0.get_mime_type().unwrap(), cur_case.1);
+    }
+
+    let err_test_cases = [(
+        File::new(Path::new("tests/files/unavialabile_file")),
+        "An IO error was thrown while trying to determine the MIME type of a file",
+    )];
+    for cur_case in err_test_cases.iter() {
+        assert!(cur_case
+            .0
+            .get_mime_type()
+            .unwrap_err()
+            .to_string()
+            .contains(cur_case.1));
+    }
+}
+
+#[cfg(target_os = "macos")]
+#[test]
+fn test_is_of_type_macos() {
+    let ok_test_cases = [
+        (
+            File::new(Path::new("tests/files/audio/wav")),
+            "application/x-riff",
+        ),
         (
             File::new(Path::new("tests/files/audio/ogg")),
             "audio/x-vorbis+ogg",
@@ -51,6 +74,35 @@ fn test_is_of_type() {
             File::new(Path::new("tests/files/audio/opus")),
             "audio/x-opus+ogg",
         ),
+    ];
+    for cur_case in ok_test_cases.iter() {
+        assert_eq!(cur_case.0.get_mime_type().unwrap(), cur_case.1);
+    }
+
+    let err_test_cases = [(
+        File::new(Path::new("tests/files/unavialabile_file")),
+        "An IO error was thrown while trying to determine the MIME type of a file",
+    )];
+    for cur_case in err_test_cases.iter() {
+        assert!(cur_case
+            .0
+            .get_mime_type()
+            .unwrap_err()
+            .to_string()
+            .contains(cur_case.1));
+    }
+}
+
+#[cfg(target_os = "linux")]
+#[test]
+fn test_is_of_type_linux() {
+    let ok_test_cases = [
+        (
+            File::new(Path::new("tests/files/audio/wav")),
+            "audio/vnd.wave",
+        ),
+        (File::new(Path::new("tests/files/audio/ogg")), "video/ogg"),
+        (File::new(Path::new("tests/files/audio/opus")), "video/ogg"),
     ];
     for cur_case in ok_test_cases.iter() {
         assert_eq!(cur_case.0.get_mime_type().unwrap(), cur_case.1);
